e02ba0739b97e7d0cf8bcb848ccfb9a36d52da5e
Al Capitan <alexandre.boyer29@gmail.com>
2022-04-12 12:04:01 +0200

    Diverses améliorations

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
-function sendRequest()
+function sendRequest(request)
 {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-      test = this.responseText;
-      console.log(test);
-      }
-    xhttp.open("GET", "http://api.meteo-concept.com/api/location/cities?token=8d432d87acf6b2a4e36ee21cd41d5821cb1db3133b673e79dd0f6f0b80cca53f&search=Orange", true);
-    xhttp.send()
+     const xhttp = new XMLHttpRequest();
+     xhttp.onload = function() {
+          test = this.responseText;
+          console.log(test);
+     }
+     xhttp.open("GET", request, true);
+     xhttp.send()
 }
 
 function createRequest()
 {
-    var inputVille = document.getElementById("inputVille");
-    var inputDate = document.getElementById("inputDate");
-    inputVille = inputVille.value;
-    inputDate = inputDate.value;
-    console.log([inputVille,inputDate]);
-    var request = "http://api.meteo-concept.com/api/location/cities?token=8d432d87acf6b2a4e36ee21cd41d5821cb1db3133b673e79dd0f6f0b80cca53f&search=Orange";
-    sendRequest();
+     var inputVille = document.getElementById("inputVille");
+     var inputDate = document.getElementById("inputDate");
+     inputVille = inputVille.value;
+     inputDate = inputDate.value;
+     console.log([inputVille,inputDate]);
+     var request = "http://api.meteo-concept.com/api/location/cities?token=8d432d87acf6b2a4e36ee21cd41d5821cb1db3133b673e79dd0f6f0b80cca53f&search=Orange";
+     console.log(request);
+     sendRequest(request);
 }
 
 var searchButton = document.getElementById("search");
