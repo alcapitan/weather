33b270f439cb84179c86d2bb245ee2557b616104
Al Capitan <alexandre.boyer29@gmail.com>
2022-04-12 13:06:58 +0200

    Ajout de requestHeader

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 function sendRequest(request)
 {
-     var rep;
-     var request = new XMLHttpRequest();
-     request.open('GET',request);
-     request.responseType = 'json';
-     request.onload = function() {
-          rep = request.response;
-        };
-        console.log("rep:",rep);
-     request.send()
+     var requestHeader = new Headers();
+     requestHeader.append('Content-Type', 'application/json');
+     requestHeader.append('Access-Control-Allow-Origin', 'no-cors');
+     requestHeader.append('method', 'GET');
+     requestHeader.get('Content-Type');
+     requestHeader.get('Access-Control-Allow-Origin');
+     requestHeader.get('method');
+     console.log(requestHeader);
+     fetch(request,{requestHeader});
 }
 
 function createRequest()
 {
-     var inputVille = document.getElementById("inputVille");
+     var inputCity = document.getElementById("inputCity");
      var inputDate = document.getElementById("inputDate");
-     inputVille = inputVille.value;
+     inputCity = inputCity.value;
      inputDate = inputDate.value;
-     console.log([inputVille,inputDate]);
-     var request = "http://api.meteo-concept.com/api/location/cities?token=8d432d87acf6b2a4e36ee21cd41d5821cb1db3133b673e79dd0f6f0b80cca53f&search="+inputVille;
+     console.log([inputCity,inputDate]);
+     var request = "http://api.meteo-concept.com/api/location/cities?token=8d432d87acf6b2a4e36ee21cd41d5821cb1db3133b673e79dd0f6f0b80cca53f&search="+inputCity;
      console.log(request);
      sendRequest(request);
 }
