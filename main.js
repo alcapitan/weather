a2d1c397eee3739045e9bdb53670537bda46243a
Al Capitan <alexandre.boyer29@gmail.com>
2022-04-12 16:39:44 +0200

    Stabilisation des requêtes et traitements

--- main.js
+++ main.js
@@ -1,43 +1,54 @@
-async function sendRequest(request)
+async function sendRequest(city)
 {
+     // Header des requêtes
      var requestHeader = new Headers({
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer 8d432d87acf6b2a4e36ee21cd41d5821cb1db3133b673e79dd0f6f0b80cca53f' 
-    });
-
-     var inputCity = document.getElementById("inputCity");
-     inputCity = inputCity.value;
-     const response = await fetch(`https://api.meteo-concept.com/api/location/cities?token=8d432d87acf6b2a4e36ee21cd41d5821cb1db3133b673e79dd0f6f0b80cca53f&search=${inputCity}`,
-        {
-            method: 'GET',
-            headers: requestHeader,
-            mode:'cors'
-        });
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer 8d432d87acf6b2a4e36ee21cd41d5821cb1db3133b673e79dd0f6f0b80cca53f' 
+     });
+     // Réception villes possibles parmi recherche
+     var response = await fetch(`https://api.meteo-concept.com/api/location/cities?token=8d432d87acf6b2a4e36ee21cd41d5821cb1db3133b673e79dd0f6f0b80cca53f&search=${city}`,
+     {
+          method: 'GET',
+          headers: requestHeader,
+          mode:'cors'
+     });
+     var content = await response.json(); // Converti la réponse en JSON
+     var insee = content.cities[0]["insee"]; // Révèle l'INSEE de la ville en premier résultat
+     // Réception des infos sur la ville
+     var responseInsee = await fetch(`https://api.meteo-concept.com/api/forecast/daily?token=8d432d87acf6b2a4e36ee21cd41d5821cb1db3133b673e79dd0f6f0b80cca53f&insee=${insee}`,
+     {
+          method: 'GET',
+          headers: requestHeader,
+          mode:'cors'
+     });
+     var contentInsee = await responseInsee.json(); // Converti la réponse en JSON
+     var result = {}; // Créé un objet avec toutes les infos nécessaires
+     result["codePostal"] = contentInsee.forecast[0]["cp"];
+     result["kmwind"] = contentInsee.forecast[0]["wind10m"];
+     result["dirwind"] = contentInsee.forecast[0]["dirwind10m"];
+     result["weather"] = contentInsee.forecast[0]["weather"];
+     result["temperature"] = (contentInsee.forecast[0]["tmin"] + contentInsee.forecast[0]["tmax"]) / 2;
+     updateDisplay(result);
 }
 
 function createRequest()
 {
      var inputCity = document.getElementById("inputCity");
-     var inputDate = document.getElementById("inputDate");
      inputCity = inputCity.value;
-     inputDate = inputDate.value;
-     console.log([inputCity,inputDate]);
-     var request = "http://api.meteo-concept.com/api/location/cities?token=8d432d87acf6b2a4e36ee21cd41d5821cb1db3133b673e79dd0f6f0b80cca53f&search="+inputCity;
-     console.log(request);
-     sendRequest(request);
+     sendRequest(inputCity);
 }
 
-function updateDisplay(temperature,sky,wind)
+function updateDisplay(infos)
 {
      // Temperature
      var temperatureElement = document.getElementById("temperature").childNodes[1];
-     temperatureElement.innerHTML = temperature + "°C";
-     if (temperature <= 10)
+     temperatureElement.innerHTML = infos["temperature"] + "°C";
+     if (infos["temperature"] <= 10)
      {
           document.body.style.backgroundColor = "rgb(51, 204, 255)";
      }
-     else if (temperature <= 21)
+     else if (infos["temperature"] <= 21)
      {
           document.body.style.backgroundColor = "rgb(255, 204, 102)";
      }
@@ -47,18 +58,16 @@ function updateDisplay(temperature,sky,wind)
      }
      // Sky
      var skyElementImage = document.getElementById("sky").childNodes[1];
-     skyElementImage.src = sky + ".png";
-     skyElementImage.alt = sky + " icon";
+     skyElementImage.src = infos["weather"] + ".png";
+     skyElementImage.alt = infos["weather"] + " icon";
      var skyElementText = document.getElementById("sky").childNodes[3];
-     skyElementText.innerHTML = sky;
+     skyElementText.innerHTML = infos["weather"];
      // Wind
      var windElementImage = document.getElementById("wind").childNodes[1];
-     windElementImage.style.transform = "rotate(70deg)";
+     windElementImage.style.transform = "rotate(" + infos["dirwind"] + "deg)";
      var windElementText = document.getElementById("wind").childNodes[3];
-     windElementText.innerHTML = wind +"km/h";
+     windElementText.innerHTML = infos["kmwind"] +"km/h";
 }
 
-updateDisplay(-10,"rain",30)
-
 var searchButton = document.getElementById("search");
-searchButton.addEventListener('click',sendRequest);
+searchButton.addEventListener('click',createRequest);
