9673be4b43469b1b088e98d8a0258a7e63165f7e
Al Capitan <alexandre.boyer29@gmail.com>
2022-04-12 11:52:56 +0200

    Ajout envoi requête

---
+++ main.js
@@ -0,0 +1,24 @@
+function sendRequest()
+{
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      test = this.responseText;
+      console.log(test);
+      }
+    xhttp.open("GET", "http://api.meteo-concept.com/api/location/cities?token=8d432d87acf6b2a4e36ee21cd41d5821cb1db3133b673e79dd0f6f0b80cca53f&search=Orange", true);
+    xhttp.send()
+}
+
+function createRequest()
+{
+    var inputVille = document.getElementById("inputVille");
+    var inputDate = document.getElementById("inputDate");
+    inputVille = inputVille.value;
+    inputDate = inputDate.value;
+    console.log([inputVille,inputDate]);
+    var request = "http://api.meteo-concept.com/api/location/cities?token=8d432d87acf6b2a4e36ee21cd41d5821cb1db3133b673e79dd0f6f0b80cca53f&search=Orange";
+    sendRequest();
+}
+
+var searchButton = document.getElementById("search");
+searchButton.addEventListener('click',createRequest);
