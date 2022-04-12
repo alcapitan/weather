6872b0fb54c27c22d036f56093b1b0c2348d78ca
Al Capitan <alexandre.boyer29@gmail.com>
2022-04-12 14:37:19 +0200

    Travail Zack

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 async function sendRequest(request)
 {
-     var requestHeader = new Headers({'Content-Type':'application/json','method':'GET','Accept':'application/json'});
+     //var requestHeader = new Headers({'Content-Type':'application/json','method':'GET','Accept':'application/json'});
      /*requestHeader.append('Content-Type', 'application/json');
      requestHeader.append('Access-Control-Allow-Origin', 'no-cors');
      requestHeader.append('method', 'GET');*/
      var inputCity = document.getElementById("inputCity");
      inputCity = inputCity.value;
-     console.log(requestHeader);
-     const response = await fetch(`http://api.meteo-concept.com/api/location/cities?token=8d432d87acf6b2a4e36ee21cd41d5821cb1db3133b673e79dd0f6f0b80cca53f&search=${inputCity}`,{headers: requestHeader,mode:'no-cors'});
-     console.log(response)
+     try{
+          let response = await fetch(`http://api.meteo-concept.com/api/location/cities?token=8d432d87acf6b2a4e36ee21cd41d5821cb1db3133b673e79dd0f6f0b80cca53f&search=${inputCity}`,
+               {headers: {'Accept':'application/json', 'Content-Type': 'application/json'},
+               mode:'no-cors',
+               method: 'GET',
+               });     
+               console.log(response)
+          }
+          catch {
+               return undefined
+          }
 }
 
 /*function createRequest()
