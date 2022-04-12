4293a8d7c83c67376d26752b9586ce804aa32356
Al Capitan <alexandre.boyer29@gmail.com>
2022-04-12 12:10:36 +0200

    Diverses améliorations

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 function sendRequest(request)
 {
-     const xhttp = new XMLHttpRequest();
-     xhttp.onload = function() {
-          test = this.responseText;
-          console.log(test);
-     }
-     xhttp.open("GET", request, true);
-     xhttp.send()
+     var rep;
+     var request = new XMLHttpRequest();
+     request.open('GET',request);
+     request.responseType = 'json';
+     request.onload = function() {
+          rep = request.response;
+        };
+        console.log("rep:",rep);
+     request.send()
 }
 
 function createRequest()
@@ -16,7 +18,7 @@ function createRequest()
      inputVille = inputVille.value;
      inputDate = inputDate.value;
      console.log([inputVille,inputDate]);
-     var request = "http://api.meteo-concept.com/api/location/cities?token=8d432d87acf6b2a4e36ee21cd41d5821cb1db3133b673e79dd0f6f0b80cca53f&search=Orange";
+     var request = "http://api.meteo-concept.com/api/location/cities?token=8d432d87acf6b2a4e36ee21cd41d5821cb1db3133b673e79dd0f6f0b80cca53f&search="+inputVille;
      console.log(request);
      sendRequest(request);
 }
