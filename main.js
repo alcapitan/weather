2c01e1219b6f8e424511c58baab5d59b8a664e41
Al Capitan <alexandre.boyer29@gmail.com>
2022-04-12 14:10:45 +0200

    Travail des autres

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-function sendRequest(request)
+async function sendRequest(request)
 {
-     var requestHeader = new Headers();
-     requestHeader.append('Content-Type', 'application/json');
+     var requestHeader = new Headers({'Content-Type':'application/json','method':'GET','Accept':'application/json'});
+     /*requestHeader.append('Content-Type', 'application/json');
      requestHeader.append('Access-Control-Allow-Origin', 'no-cors');
-     requestHeader.append('method', 'GET');
-     requestHeader.get('Content-Type');
-     requestHeader.get('Access-Control-Allow-Origin');
-     requestHeader.get('method');
+     requestHeader.append('method', 'GET');*/
+     var inputCity = document.getElementById("inputCity");
+     inputCity = inputCity.value;
      console.log(requestHeader);
-     fetch(request,{requestHeader});
+     const response = await fetch(`http://api.meteo-concept.com/api/location/cities?token=8d432d87acf6b2a4e36ee21cd41d5821cb1db3133b673e79dd0f6f0b80cca53f&search=${inputCity}`,{headers: requestHeader,mode:'no-cors'});
+     console.log(response)
 }
 
-function createRequest()
+/*function createRequest()
 {
      var inputCity = document.getElementById("inputCity");
      var inputDate = document.getElementById("inputDate");
@@ -21,7 +21,7 @@ function createRequest()
      var request = "http://api.meteo-concept.com/api/location/cities?token=8d432d87acf6b2a4e36ee21cd41d5821cb1db3133b673e79dd0f6f0b80cca53f&search="+inputCity;
      console.log(request);
      sendRequest(request);
-}
+}*/
 
 var searchButton = document.getElementById("search");
-searchButton.addEventListener('click',createRequest);
+searchButton.addEventListener('click',sendRequest);
